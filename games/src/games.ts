e92149f20918ee0f3673a5d298fd10a6c964ee65
Hunteraulo1 <hunteraulo@pm.me>
2024-06-15 15:51:11 +0200

    :bug: (games) start by 0

--- games/src/games.ts
+++ games/src/games.ts
@@ -31,43 +31,41 @@ export const games = (doc: GoogleAppsScript.Spreadsheet.Spreadsheet) => {
   console.log('call API')
 
   for (let i = 0; i < values.length; i++) {
-    if (i > 0) {
-      let hostname
+    let hostname
 
-      switch (values[i][0]?.getText().split('.')[0]) {
-        case 'F95z':
-          hostname = 'f95zone.to'
-          break
-        case 'LewdCorner':
-          hostname = 'lewdcorner.com'
-          break
-        default:
-          hostname = null
-      }
-
-      const row: Game = {
-        id: valuesId[i][0] || null,
-        domain: values[i][0]?.getText() || '',
-        hostname,
-        name: values[i][1]?.getText() || '',
-        version: values[i][2]?.getText() || '',
-        tversion: values[i][3]?.getText() || '',
-        tname: values[i][4]?.getText() || '',
-        status: values[i][5]?.getText() || '',
-        tags: values[i][6]?.getText().split(', ') || [],
-        type: values[i][7]?.getText() || '',
-        traductor: values[i][8]?.getText() || null,
-        proofreader: values[i][9]?.getText() || null,
-        ttype: values[i][10]?.getText() || '',
-        ac: Boolean(values[i][11]?.getText()),
-        image: values[i][12]?.getText() || null,
-        link: values[i][1]?.getLinkUrl() || '',
-        tlink: values[i][4]?.getLinkUrl() || '',
-        trlink: values[i][8]?.getLinkUrl() || null,
-      }
-
-      result.push(row)
+    switch (values[i][0]?.getText().split('.')[0]) {
+      case 'F95z':
+        hostname = 'f95zone.to'
+        break
+      case 'LewdCorner':
+        hostname = 'lewdcorner.com'
+        break
+      default:
+        hostname = null
     }
+
+    const row: Game = {
+      id: valuesId[i][0] || null,
+      domain: values[i][0]?.getText() || '',
+      hostname,
+      name: values[i][1]?.getText() || '',
+      version: values[i][2]?.getText() || '',
+      tversion: values[i][3]?.getText() || '',
+      tname: values[i][4]?.getText() || '',
+      status: values[i][5]?.getText() || '',
+      tags: values[i][6]?.getText().split(', ') || [],
+      type: values[i][7]?.getText() || '',
+      traductor: values[i][8]?.getText() || null,
+      proofreader: values[i][9]?.getText() || null,
+      ttype: values[i][10]?.getText() || '',
+      ac: Boolean(values[i][11]?.getText()),
+      image: values[i][12]?.getText() || null,
+      link: values[i][1]?.getLinkUrl() || '',
+      tlink: values[i][4]?.getLinkUrl() || '',
+      trlink: values[i][8]?.getLinkUrl() || null,
+    }
+
+    result.push(row)
   }
 
   return result
